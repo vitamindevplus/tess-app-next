4c29f21d934ff71e3e96fea132c6cd1f14a8df33
thiennt <thiennt@hivetech.vn>
2023-04-04 14:50:30 +0700

    feat: create new

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,16 +1,6 @@
-import "@/styles/globals.css";
-import type { AppProps } from "next/app";
-import { IBM_Plex_Mono } from "@next/font/google";
-
-const ibmPlexMono = IBM_Plex_Mono({
-  subsets: ["latin"],
-  weight: "600",
-});
+import '@/styles/globals.css'
+import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return (
-    <main className={ibmPlexMono.className}>
-      <Component {...pageProps} />
-    </main>
-  );
+  return <Component {...pageProps} />
 }
--- pages/api/auth.ts
+++
@@ -1,9 +0,0 @@
-import request from "../utils/axios";
-
-export function loginApi(data: any): any {
-  return request({
-    url: "/api/v1/auth/login",
-    method: "post",
-    data,
-  });
-}
